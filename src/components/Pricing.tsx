fd669c2eac48696dc83702d66797e19aa1a03633
Kerneels94 <corriebotha64@gmail.com>
2024-03-23 09:32:29 +0200

    Added: Footer component in the body component, Update: Pricing component

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,5 +1,6 @@
 // Imports
 import Header from "./Header"
+import Footer from "./Footer"
 
 /**
  * @description Pricing component
@@ -7,7 +8,45 @@ import Header from "./Header"
 const Pricing = () => {
   return <>
     <Header />
-    <div>Pricing</div>
+    <div className="mx-auto p-2 space-y-2">
+      <div className="heading text-center space-y-2">
+        <h1 className="text-lg">Our pricing</h1>
+        <p>We offer 3 subscription based packages</p>
+      </div>
+      <div className="card-container flex flex-col items-center justify-center space-y-4 lg:grid-cols-3 lg:items-center lg:gap-4">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
   </>
 }
 
