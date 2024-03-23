68727ad8fa8d6ee59e3642cf0248ccfc10b222df
Kerneels94 <corriebotha64@gmail.com>
2024-03-23 09:26:31 +0200

    Added: Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,40 @@
+
+const Footer = () => {
+  return <>
+     <footer className="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-col items-center justify-between">
+          <span className="text-xl text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{" "}
+            <a href="https://reports.co.za" className="hover:underline">
+              Reports™
+            </a>
+            . All Rights Reserved.
+          </span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline me-4 md:me-6">
+                Licensing
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+  </>
+}
+
+export default Footer
