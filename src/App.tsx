037d2e5d082531db3634cd4b3f64ffc7a8cae346
Kerneels94 <corriebotha64@gmail.com>
2024-03-24 09:12:46 +0200

    Added: Themecontext to wrap the application

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css'
 import "./index.css"
 import Header from "./components/Header"
 import Body from "./components/Body"
+import { ThemeProvider } from './context/themeContext'
 
 function App() {
 
   return (
     <>
-
+      <ThemeProvider>
         <Header />
         <Body />
+      </ThemeProvider>
     </>
   );
 }
