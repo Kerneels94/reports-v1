a3d3660e99af4cd81614224d970e918a27b949a6
Ra'ees <planterx26@gmail.com>
2024-03-24 14:55:15 +0200

    Added firebase login funcitonality

--- src/firebase/login.ts
+++ src/firebase/login.ts
@@ -2,13 +2,13 @@
 import fb from './auth';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
-export default function loginUser(email: string, password: string) {
+function loginUser (email: string, password: string) {
     // Firebase login in user
     createUserWithEmailAndPassword(fb.auth, email, password)
       .then((userCredential: any) => {
         console.log('Store: ', userCredential.user)
-          const user = userCredential.user;
-          return user;  
+          // const user = userCredential.user;
+          // return user;  
       })
       .catch((error: any) => {
         // todo display error popup
@@ -19,3 +19,5 @@ export default function loginUser(email: string, password: string) {
         console.error('Error Message: ' + errorMessage)
       })
 }
+
+export default { loginUser };
